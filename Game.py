1e4a3efd927a2dbffe4a7fd1f57b607c3abe88fd
Neuromorphik <kyler.msc@gmail.com>
2020-03-28 19:51:30 -0600

    minor refactoring of Game class skeleton

--- Game.py
+++ Game.py
@@ -3,23 +3,35 @@ import Board
 
 class Game:
 
-    def __init__(self, ui_manager, input_manager, board_size=Board.DEFAULT_BOARD_SIZE,
+    def __init__(self, ui_manager, board_size=Board.DEFAULT_BOARD_SIZE,
                     ship_ship=Board.DEFAULT_SHIP_SIZE):
         self.ui = ui_manager
-        self.input = input_manager
-        self.board_player_1 = Board.Board(board_size)
-        self.board_player_2 = Board.Board(board_size)
+        self.board_player_1 = Board.Board(board_size=board_size, ship_size=ship_size)
+        self.board_player_2 = Board.Board(board_size=board_size, ship_size=ship_size)
+
+    def show_menu(self):
+        choice = self.ui.get_menu_selection()
+        if choice == 'quit':
+            self.ui.show_exit_message()
+        elif choice == 'play':
+            self.start_game()
+        else:
+            pass
 
     def start_game(self):
         self.turn = 1
+        self.game_over = False
         self.board_player_1.reset_board()
         self.board_player_2.reset_board()
+        self.ui.show_start_message()
 
-        while True:
-            # clear console
+        while not self.game_over:
+            # clear console to keep privacy between players
 
+            # handle p1 turn
             if (self.turn == 1):
                 # show p1 their interface
+                self.ui.show_turn_message(turn=1)
                 self.ui.show_self_board(self.board_player_1)
                 self.ui.show_other_board(self.board_player_2)
 
@@ -37,13 +49,15 @@ class Game:
 
                 # end game if p2 ship destroyed
                 if self.board_player_2.ship_destroyed():
-                    return
+                    self.game_over = True
 
                 # set turn to p2
                 self.turn = 2
 
+            # handle p2 turn
             else:
                 # show p2 their interface
+                self.ui.show_turn_message(turn=2)
                 self.ui.show_self_board(self.board_player_2)
                 self.ui.show_other_board(self.board_player_1)
 
@@ -61,7 +75,9 @@ class Game:
 
                 # end game if p1 ship destroyed
                 if self.board_player_1.ship_destroyed():
-                    return
+                    self.game_over = True
 
                 # set turn to p1
                 self.turn = 1
+
+        self.show_menu()
